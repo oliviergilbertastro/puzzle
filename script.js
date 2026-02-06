5a445214481a9d8137fa9ed93d9321e04942684f
Olivier Desktop <ogilbert@umich.edu>
2026-02-06 18:45:18 -0500

    ball + grid works fine

--- script.js
+++ script.js
@@ -2,23 +2,87 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 1000;
 canvas.height = 500
-
+const UNIT = 50; // 1 unit = 50 pixels
 
 document.getElementById("runBtn").addEventListener("click", run);
+
+
+class Ball {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = 10;
+    }
+
+    draw() {
+        const grad = ctx.createRadialGradient(
+            canvas.width/2 - this.radius * 0.3,
+            canvas.height/2 - this.radius * 0.3,
+            this.radius * 0.2,
+            canvas.width/2,
+            canvas.height/2,
+            this.radius
+        );
+
+        grad.addColorStop(0, "#b9adff");   // highlight
+        grad.addColorStop(0.5, "#6a5cff");
+        grad.addColorStop(1, "#3a2dbd");   // shadow
+
+        ctx.beginPath();
+        ctx.arc(canvas.width/2, canvas.height/2, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = grad;
+        ctx.fill();
+    }
+
+    
+}
+
+
 
 function run() {
     const val1 = document.getElementById("inputX").value;
     const val2 = document.getElementById("inputY").value;
     console.log("Running with inputs:", val1, val2)
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     // Example drawing
     ctx.fillStyle = "#4a90e2";
     ctx.fillRect(50, 50, 200, 100);
-
+    ball.x += 0.1;
+    ball.y += 0.2;
     console.log("Inputs:", val1, val2);
+}
+
+function make_grids(posx, posy) {
+    // if position of ball is (3,4), then we want to plot grid
+    // lines from 3*UNIT-500 to 3*UNIT+500, and from 4*UNIT-500 to 4*UNIT+500
+    // but we want (0,0) to be at the center of the canvas, so we need to shift everything by canvas.width/2 and canvas.height/2
+    ctx.strokeStyle = "#ccc";
+    ctx.linewidth = 5;
+    console.log(posx % UNIT)
+    for (let x = (posx % 1)*UNIT-UNIT; x < canvas.width+UNIT; x += UNIT) {
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, canvas.height);
+        ctx.stroke();
+    }
+    for (let y = -(posy % 1)*UNIT-UNIT; y < canvas.height+UNIT; y += UNIT) {
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(canvas.width, y);
+        ctx.stroke();
+    }
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    make_grids(ball.x, ball.y);
+    ball.draw();
 }
+
+ball = new Ball(0, 0);
+
+setInterval(draw, 10);
+setInterval(function() {
+    ball.x += 0.01;
+    ball.y += 0.02;
+}, 10);
